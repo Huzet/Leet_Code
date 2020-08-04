3598aab03e2f2bcd6594d222051c3eb774176489
Tomas <tomas.huzevka@outlook.com>
2020-08-03 20:22:53 -0400

    challenge 2 added

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
+# Python3
+import re
 
- 
+searchstring = ('hello, HELLO, helLO')
+
+myregex = re.compile(r'hel')
+result = myregex.sub('HEL', searchstring)
+print(result)
+
+re.compile(r'''(
+    (\d{3}|\(\d{3}\))   #area code
+    \d{4}               #last 4 digits
+)''', re.VERBOSE)
