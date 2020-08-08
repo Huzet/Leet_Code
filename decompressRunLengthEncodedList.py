f5a401ca18be7ec6db5f4a9b61778f6a79b8cfbb
Tomas <tomas.huzevka@outlook.com>
2020-08-07 23:00:52 -0400

    decompressRunLengthEncodedList.py

---
+++ decompressRunLengthEncodedList.py
@@ -0,0 +1,24 @@
+#! Python3
+
+# https://leetcode.com/problems/decompress-run-length-encoded-list/
+
+nums = [1,1,2,3]
+# Output: [1,3,3]
+
+def decompressRLElist(nums):
+    newlist = []
+    for x in range(0, len(nums), 2):
+        counter = nums[x] 
+        print(counter)
+        while counter != 0:
+            newlist.append(nums[x+1])
+            counter = counter - 1
+    return newlist
+        
+
+decompressRLElist(nums)
+
+# Success
+# Details
+# Runtime: 80 ms, faster than 42.66% of Python3 online submissions for Decompress Run-Length Encoded List.
+# Memory Usage: 13.7 MB, less than 99.84% of Python3 online submissions for Decompress Run-Length Encoded List.
