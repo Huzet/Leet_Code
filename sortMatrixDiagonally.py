c6f248851e598b043c4085df92aebffb33b362a6
Tomas <tomas.huzevka@outlook.com>
2020-08-13 23:12:03 -0400

    made to do list

---
+++ sortMatrixDiagonally.py
@@ -0,0 +1,20 @@
+#! Python3
+
+# https://leetcode.com/problems/sort-the-matrix-diagonally/
+
+# Input: 
+mat = [ [3,3,1,1],
+        [2,2,1,2],
+        [1,1,1,2]]
+
+# Output: [ [1,1,1,1],
+#           [1,2,2,2],
+#           [1,2,3,3]]
+
+def diagonalSort(mat):
+    # 1. make diagonal lists
+    # 2. organize diagonal lists
+    # 3. convert back into list
+
+
+diagonalSort(mat)
