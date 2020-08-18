843fe064183503dd0e76ddfd4e8a27c90dfa2b8c
Tomas <tomas.huzevka@outlook.com>
2020-08-17 23:23:00 -0400

    still working it

--- sortMatrixDiagonally.py
+++ sortMatrixDiagonally.py
@@ -2,6 +2,8 @@
 
 # https://leetcode.com/problems/sort-the-matrix-diagonally/
 
+import re
+
 # Input: 
 mat = [ [3,3,1,1],
         [2,2,1,2],
@@ -11,13 +13,14 @@ compare = [3,2,1,3,1,2,1,2,1,2,1,1]
 
 # 1. Seperate diagonaly
 # 2. Group in diagonal groups
-# [[3,2,1],
-# [3,1,2],
-# [1,2],
-# [1],
-# [2,1],
-# [1]]
+# [[3,2,1],     3
+# [3,1,2],      3
+# [1,2],        2
+# [1],          1
+# [2,1],        2
+# [1]]          1
 # 
+# 4 X 3 
 # 
 
 # Output: [ [1,1,1,1],  00 01 02 03 XX    1
@@ -31,26 +34,41 @@ def diagonalSort(mat, compare):
     # 1. make diagonal lists
     diagonalList = []
     counter = 0
+    blankline = []
     # for _ in range(int(len(mat) + len(mat[0]) - 1)):
     #     diagonalList.append([])
     for y in range(len(mat)):
-    
+ 
         for x in range(len(mat[y])):    
-            counter = counter + 1
+            
             for diag in range(len(mat)): 
                 if y == 0:
                     try:
+                        # diagonalList[counter].append(mat[y+diag][x+diag])
                         diagonalList.append(mat[y+diag][x+diag])
-                         
+                        if counter == len(mat):
+                            counter = 1
+                            blankline.append('newline')
+                        else:
+                            counter = counter + 1
+                        blankline.append(counter)
                     except:
-
+                        counter = 0
+                        blankline.append('newline')
                         continue
                 elif y > 0 and x < 1:
                     try:
+                        # diagonalList[counter].append(mat[y+diag][x+diag])
                         diagonalList.append(mat[y+diag][x+diag])
-                        
+                        if counter == len(mat):
+                            counter = 1
+                            blankline.append('newline')
+                        else:
+                            counter = counter + 1
+                        blankline.append(counter)
                     except:
-
+                        counter = 0
+                        blankline.append('newline')
                         continue
                             
     
@@ -60,14 +78,15 @@ def diagonalSort(mat, compare):
     # else:
     #     print('does not match')
     # print("length of list is", + len(diagonalList))
+    print(diagonalList)
+    print(counter)
+    x = " ".join(str(blankline))
+    print(x)
 
-    # print(counter)
 
     # 2. organize diagonal lists
-    sortedDiagonalList= sorted(diagonalList)
-    print(sortedDiagonalList)
-    # 3. convert back into list
 
+    # 3. convert back into list 
     # 4. return value
 
 diagonalSort(mat, compare)
